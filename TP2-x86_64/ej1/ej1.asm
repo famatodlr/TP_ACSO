abca5e25460f97cb7b45d708cabd82b4d9b26be3
Amato de Lusarreta, Franco <famatodelusarreta@udesa.edu.ar>
2025-04-16 14:50:46 -0300

    Update ej1.asm

--- TP2-x86_64/ej1/ej1.asm
+++ TP2-x86_64/ej1/ej1.asm
@@ -1,104 +1,97 @@
 
 
-
 string_proc_list_create_asm:
         push    rbp
         mov     rbp, rsp
         sub     rsp, 16
         mov     edi, 16
         call    malloc
-        mov     QWORD PTR [rbp-8], rax
-        cmp     QWORD PTR [rbp-8], 0
+        mov     [rbp-8], rax
+        cmp     qword [rbp-8], 0
         jne     .L2
         mov     eax, 0
         jmp     .L3
 .L2:
-        mov     rax, QWORD PTR [rbp-8]
-        mov     QWORD PTR [rax], 0
-        mov     rax, QWORD PTR [rbp-8]
-        mov     QWORD PTR [rax+8], 0
-        mov     rax, QWORD PTR [rbp-8]
+        mov     rax, [rbp-8]
+        mov     qword [rax], 0
+        mov     rax, [rbp-8]
+        mov     qword [rax+8], 0
+        mov     rax, [rbp-8]
 .L3:
         leave
         ret
-
-
-
 
 string_proc_node_create_asm:
         push    rbp
         mov     rbp, rsp
         sub     rsp, 32
         mov     eax, edi
-        mov     QWORD PTR [rbp-32], rsi
-        mov     BYTE PTR [rbp-20], al
+        mov     [rbp-32], rsi
+        mov     byte [rbp-20], al
         mov     edi, 32
         call    malloc
-        mov     QWORD PTR [rbp-8], rax
-        cmp     QWORD PTR [rbp-8], 0
+        mov     [rbp-8], rax
+        cmp     qword [rbp-8], 0
         jne     .L5
         mov     eax, 0
         jmp     .L6
 .L5:
-        mov     rax, QWORD PTR [rbp-8]
-        mov     QWORD PTR [rax], 0
-        mov     rax, QWORD PTR [rbp-8]
-        mov     QWORD PTR [rax+8], 0
-        mov     rax, QWORD PTR [rbp-8]
-        mov     rdx, QWORD PTR [rbp-32]
-        mov     QWORD PTR [rax+24], rdx
-        mov     rax, QWORD PTR [rbp-8]
-        movzx   edx, BYTE PTR [rbp-20]
-        mov     BYTE PTR [rax+16], dl
-        mov     rax, QWORD PTR [rbp-8]
+        mov     rax, [rbp-8]
+        mov     qword [rax], 0
+        mov     rax, [rbp-8]
+        mov     qword [rax+8], 0
+        mov     rax, [rbp-8]
+        mov     rdx, [rbp-32]
+        mov     qword [rax+24], rdx
+        mov     rax, [rbp-8]
+        movzx   edx, byte [rbp-20]
+        mov     byte [rax+16], dl
+        mov     rax, [rbp-8]
 .L6:
         leave
         ret
-
-
-
 
 string_proc_list_add_node_asm:
         push    rbp
         mov     rbp, rsp
         sub     rsp, 48
-        mov     QWORD PTR [rbp-24], rdi
+        mov     [rbp-24], rdi
         mov     eax, esi
-        mov     QWORD PTR [rbp-40], rdx
-        mov     BYTE PTR [rbp-28], al
-        cmp     QWORD PTR [rbp-24], 0
+        mov     [rbp-40], rdx
+        mov     byte [rbp-28], al
+        cmp     qword [rbp-24], 0
         je      .L12
-        movzx   eax, BYTE PTR [rbp-28]
-        mov     rdx, QWORD PTR [rbp-40]
+        movzx   eax, byte [rbp-28]
+        mov     rdx, [rbp-40]
         mov     rsi, rdx
         mov     edi, eax
         call    string_proc_node_create
-        mov     QWORD PTR [rbp-8], rax
-        cmp     QWORD PTR [rbp-8], 0
+        mov     [rbp-8], rax
+        cmp     qword [rbp-8], 0
         je      .L13
-        mov     rax, QWORD PTR [rbp-24]
-        mov     rax, QWORD PTR [rax]
+        mov     rax, [rbp-24]
+        mov     rax, [rax]
         test    rax, rax
         jne     .L11
-        mov     rax, QWORD PTR [rbp-24]
-        mov     rdx, QWORD PTR [rbp-8]
-        mov     QWORD PTR [rax], rdx
-        mov     rax, QWORD PTR [rbp-24]
-        mov     rdx, QWORD PTR [rbp-8]
-        mov     QWORD PTR [rax+8], rdx
+        mov     rax, [rbp-24]
+        mov     rdx, [rbp-8]
+        mov     [rax], rdx
+        mov     rax, [rbp-24]
+        mov     rdx, [rbp-8]
+        mov     [rax+8], rdx
         jmp     .L7
 .L11:
-        mov     rax, QWORD PTR [rbp-24]
-        mov     rax, QWORD PTR [rax+8]
-        mov     rdx, QWORD PTR [rbp-8]
-        mov     QWORD PTR [rax], rdx
-        mov     rax, QWORD PTR [rbp-24]
-        mov     rdx, QWORD PTR [rax+8]
-        mov     rax, QWORD PTR [rbp-8]
-        mov     QWORD PTR [rax+8], rdx
-        mov     rax, QWORD PTR [rbp-24]
-        mov     rdx, QWORD PTR [rbp-8]
-        mov     QWORD PTR [rax+8], rdx
+        mov     rax, [rbp-24]
+        mov     rax, [rax+8]
+        mov     rdx, [rbp-8]
+        mov     [rax], rdx
+        mov     rax, [rbp-24]
+        mov     rdx, [rax+8]
+        mov     rax, [rbp-8]
+        mov     [rax+8], rdx
+        mov     rax, [rbp-24]
+        mov     rdx, [rbp-8]
+        mov     [rax+8], rdx
         jmp     .L7
 .L12:
         nop
@@ -109,98 +102,96 @@ string_proc_list_add_node_asm:
         leave
         ret
 
-
-
-
 string_proc_list_concat_asm:
         push    rbp
         mov     rbp, rsp
         sub     rsp, 64
-        mov     QWORD PTR [rbp-40], rdi
+        mov     [rbp-40], rdi
         mov     eax, esi
-        mov     QWORD PTR [rbp-56], rdx
-        mov     BYTE PTR [rbp-44], al
-        cmp     QWORD PTR [rbp-40], 0
+        mov     [rbp-56], rdx
+        mov     byte [rbp-44], al
+        cmp     qword [rbp-40], 0
         je      .L15
-        cmp     QWORD PTR [rbp-56], 0
+        cmp     qword [rbp-56], 0
         jne     .L16
 .L15:
         mov     eax, 0
         jmp     .L17
 .L16:
-        mov     rax, QWORD PTR [rbp-56]
+        mov     rax, [rbp-56]
         mov     rdi, rax
         call    strlen
-        mov     QWORD PTR [rbp-8], rax
-        mov     rax, QWORD PTR [rbp-40]
-        mov     rax, QWORD PTR [rax]
-        mov     QWORD PTR [rbp-16], rax
+        mov     [rbp-8], rax
+        mov     rax, [rbp-40]
+        mov     rax, [rax]
+        mov     [rbp-16], rax
         jmp     .L18
 .L20:
-        mov     rax, QWORD PTR [rbp-16]
-        movzx   eax, BYTE PTR [rax+16]
-        cmp     BYTE PTR [rbp-44], al
+        mov     rax, [rbp-16]
+        movzx   eax, byte [rax+16]
+        cmp     byte [rbp-44], al
         jne     .L19
-        mov     rax, QWORD PTR [rbp-16]
-        mov     rax, QWORD PTR [rax+24]
+        mov     rax, [rbp-16]
+        mov     rax, [rax+24]
         test    rax, rax
         je      .L19
-        mov     rax, QWORD PTR [rbp-16]
-        mov     rax, QWORD PTR [rax+24]
+        mov     rax, [rbp-16]
+        mov     rax, [rax+24]
         mov     rdi, rax
         call    strlen
-        add     QWORD PTR [rbp-8], rax
+        add     [rbp-8], rax
 .L19:
-        mov     rax, QWORD PTR [rbp-16]
-        mov     rax, QWORD PTR [rax]
-        mov     QWORD PTR [rbp-16], rax
+        mov     rax, [rbp-16]
+        mov     rax, [rax]
+        mov     [rbp-16], rax
 .L18:
-        cmp     QWORD PTR [rbp-16], 0
+        cmp     qword [rbp-16], 0
         jne     .L20
-        mov     rax, QWORD PTR [rbp-8]
+        mov     rax, [rbp-8]
         add     rax, 1
         mov     rdi, rax
         call    malloc
-        mov     QWORD PTR [rbp-24], rax
-        cmp     QWORD PTR [rbp-24], 0
+        mov     [rbp-24], rax
+        cmp     qword [rbp-24], 0
         jne     .L21
         mov     eax, 0
         jmp     .L17
 .L21:
-        mov     rax, QWORD PTR [rbp-24]
-        mov     BYTE PTR [rax], 0
-        mov     rdx, QWORD PTR [rbp-56]
-        mov     rax, QWORD PTR [rbp-24]
+        mov     rax, [rbp-24]
+        mov     byte [rax], 0
+        mov     rdx, [rbp-56]
+        mov     rax, [rbp-24]
         mov     rsi, rdx
         mov     rdi, rax
         call    strcat
-        mov     rax, QWORD PTR [rbp-40]
-        mov     rax, QWORD PTR [rax]
-        mov     QWORD PTR [rbp-16], rax
+        mov     rax, [rbp-40]
+        mov     rax, [rax]
+        mov     [rbp-16], rax
         jmp     .L22
 .L24:
-        mov     rax, QWORD PTR [rbp-16]
-        movzx   eax, BYTE PTR [rax+16]
-        cmp     BYTE PTR [rbp-44], al
+        mov     rax, [rbp-16]
+        movzx   eax, byte [rax+16]
+        cmp     byte [rbp-44], al
         jne     .L23
-        mov     rax, QWORD PTR [rbp-16]
-        mov     rax, QWORD PTR [rax+24]
+        mov     rax, [rbp-16]
+        mov     rax, [rax+24]
         test    rax, rax
         je      .L23
-        mov     rax, QWORD PTR [rbp-16]
-        mov     rdx, QWORD PTR [rax+24]
-        mov     rax, QWORD PTR [rbp-24]
+        mov     rax, [rbp-16]
+        mov     rdx, [rax+24]
+        mov     rax, [rbp-24]
         mov     rsi, rdx
         mov     rdi, rax
         call    strcat
 .L23:
-        mov     rax, QWORD PTR [rbp-16]
-        mov     rax, QWORD PTR [rax]
-        mov     QWORD PTR [rbp-16], rax
+        mov     rax, [rbp-16]
+        mov     rax, [rax]
+        mov     [rbp-16], rax
 .L22:
-        cmp     QWORD PTR [rbp-16], 0
+        cmp     qword [rbp-16], 0
         jne     .L24
-        mov     rax, QWORD PTR [rbp-24]
+        mov     rax, [rbp-24]
 .L17:
         leave
         ret
+
