1035da217e6bc9dfdabb0ba9bc78aa672c65665c
Amato de Lusarreta, Franco <famatodelusarreta@udesa.edu.ar>
2025-04-16 14:55:59 -0300

    Update ej1.asm

--- TP2-x86_64/ej1/ej1.asm
+++ TP2-x86_64/ej1/ej1.asm
@@ -1,197 +1,148 @@
 
 
 string_proc_list_create_asm:
-        push    rbp
-        mov     rbp, rsp
-        sub     rsp, 16
-        mov     edi, 16
-        call    malloc
-        mov     [rbp-8], rax
-        cmp     qword [rbp-8], 0
-        jne     .L2
-        mov     eax, 0
-        jmp     .L3
-.L2:
-        mov     rax, [rbp-8]
-        mov     qword [rax], 0
-        mov     rax, [rbp-8]
-        mov     qword [rax+8], 0
-        mov     rax, [rbp-8]
-.L3:
-        leave
-        ret
+    push    rbp
+    mov     rbp, rsp
+    mov     edi, 16
+    call    malloc
+    test    rax, rax
+    je      .fail
+    mov     qword [rax], 0        ; first = NULL
+    mov     qword [rax+8], 0      ; last = NULL
+    jmp     .done
+.fail:
+    xor     rax, rax
+.done:
+    pop     rbp
+    ret
+
+
 
 string_proc_node_create_asm:
-        push    rbp
-        mov     rbp, rsp
-        sub     rsp, 32
-        mov     eax, edi
-        mov     [rbp-32], rsi
-        mov     byte [rbp-20], al
-        mov     edi, 32
-        call    malloc
-        mov     [rbp-8], rax
-        cmp     qword [rbp-8], 0
-        jne     .L5
-        mov     eax, 0
-        jmp     .L6
-.L5:
-        mov     rax, [rbp-8]
-        mov     qword [rax], 0
-        mov     rax, [rbp-8]
-        mov     qword [rax+8], 0
-        mov     rax, [rbp-8]
-        mov     rdx, [rbp-32]
-        mov     qword [rax+24], rdx
-        mov     rax, [rbp-8]
-        movzx   edx, byte [rbp-20]
-        mov     byte [rax+16], dl
-        mov     rax, [rbp-8]
-.L6:
-        leave
-        ret
+    push    rbp
+    mov     rbp, rsp
+    mov     eax, edi                  ; type (uint8_t)
+    mov     rdx, rsi                  ; hash (char *)
+    mov     edi, 32
+    call    malloc
+    test    rax, rax
+    je      .fail
+    mov     qword [rax], 0           ; prev = NULL
+    mov     qword [rax+8], 0         ; next = NULL
+    mov     byte [rax+16], al        ; type
+    mov     qword [rax+24], rdx      ; hash
+    jmp     .done
+.fail:
+    xor     rax, rax
+.done:
+    pop     rbp
+    ret
+
+
 
 string_proc_list_add_node_asm:
-        push    rbp
-        mov     rbp, rsp
-        sub     rsp, 48
-        mov     [rbp-24], rdi
-        mov     eax, esi
-        mov     [rbp-40], rdx
-        mov     byte [rbp-28], al
-        cmp     qword [rbp-24], 0
-        je      .L12
-        movzx   eax, byte [rbp-28]
-        mov     rdx, [rbp-40]
-        mov     rsi, rdx
-        mov     edi, eax
-        call    string_proc_node_create
-        mov     [rbp-8], rax
-        cmp     qword [rbp-8], 0
-        je      .L13
-        mov     rax, [rbp-24]
-        mov     rax, [rax]
-        test    rax, rax
-        jne     .L11
-        mov     rax, [rbp-24]
-        mov     rdx, [rbp-8]
-        mov     [rax], rdx
-        mov     rax, [rbp-24]
-        mov     rdx, [rbp-8]
-        mov     [rax+8], rdx
-        jmp     .L7
-.L11:
-        mov     rax, [rbp-24]
-        mov     rax, [rax+8]
-        mov     rdx, [rbp-8]
-        mov     [rax], rdx
-        mov     rax, [rbp-24]
-        mov     rdx, [rax+8]
-        mov     rax, [rbp-8]
-        mov     [rax+8], rdx
-        mov     rax, [rbp-24]
-        mov     rdx, [rbp-8]
-        mov     [rax+8], rdx
-        jmp     .L7
-.L12:
-        nop
-        jmp     .L7
-.L13:
-        nop
-.L7:
-        leave
-        ret
+    push    rbp
+    mov     rbp, rsp
+    mov     rdx, rsi      ; type (uint8_t)
+    mov     rcx, rdx      ; copiar type en rcx para después
+    mov     rdx, rdx      ; redundante pero clara: rdx = hash
+    test    rdi, rdi
+    je      .done
+    mov     esi, ecx
+    call    string_proc_node_create
+    test    rax, rax
+    je      .done
+    mov     rbx, rdi                  ; lista
+    mov     rcx, [rbx]               ; lista->first
+    test    rcx, rcx
+    je      .empty_list
+    ; lista no vacía
+    mov     rdx, [rbx+8]             ; lista->last
+    mov     [rdx], rax               ; last->next = nuevo
+    mov     [rax+8], rdx             ; nuevo->prev = last
+    mov     [rbx+8], rax             ; lista->last = nuevo
+    jmp     .done
+.empty_list:
+    mov     [rbx], rax               ; lista->first = nuevo
+    mov     [rbx+8], rax             ; lista->last = nuevo
+.done:
+    pop     rbp
+    ret
+
+
+
 
 string_proc_list_concat_asm:
-        push    rbp
-        mov     rbp, rsp
-        sub     rsp, 64
-        mov     [rbp-40], rdi
-        mov     eax, esi
-        mov     [rbp-56], rdx
-        mov     byte [rbp-44], al
-        cmp     qword [rbp-40], 0
-        je      .L15
-        cmp     qword [rbp-56], 0
-        jne     .L16
-.L15:
-        mov     eax, 0
-        jmp     .L17
-.L16:
-        mov     rax, [rbp-56]
-        mov     rdi, rax
-        call    strlen
-        mov     [rbp-8], rax
-        mov     rax, [rbp-40]
-        mov     rax, [rax]
-        mov     [rbp-16], rax
-        jmp     .L18
-.L20:
-        mov     rax, [rbp-16]
-        movzx   eax, byte [rax+16]
-        cmp     byte [rbp-44], al
-        jne     .L19
-        mov     rax, [rbp-16]
-        mov     rax, [rax+24]
-        test    rax, rax
-        je      .L19
-        mov     rax, [rbp-16]
-        mov     rax, [rax+24]
-        mov     rdi, rax
-        call    strlen
-        add     [rbp-8], rax
-.L19:
-        mov     rax, [rbp-16]
-        mov     rax, [rax]
-        mov     [rbp-16], rax
-.L18:
-        cmp     qword [rbp-16], 0
-        jne     .L20
-        mov     rax, [rbp-8]
-        add     rax, 1
-        mov     rdi, rax
-        call    malloc
-        mov     [rbp-24], rax
-        cmp     qword [rbp-24], 0
-        jne     .L21
-        mov     eax, 0
-        jmp     .L17
-.L21:
-        mov     rax, [rbp-24]
-        mov     byte [rax], 0
-        mov     rdx, [rbp-56]
-        mov     rax, [rbp-24]
-        mov     rsi, rdx
-        mov     rdi, rax
-        call    strcat
-        mov     rax, [rbp-40]
-        mov     rax, [rax]
-        mov     [rbp-16], rax
-        jmp     .L22
-.L24:
-        mov     rax, [rbp-16]
-        movzx   eax, byte [rax+16]
-        cmp     byte [rbp-44], al
-        jne     .L23
-        mov     rax, [rbp-16]
-        mov     rax, [rax+24]
-        test    rax, rax
-        je      .L23
-        mov     rax, [rbp-16]
-        mov     rdx, [rax+24]
-        mov     rax, [rbp-24]
-        mov     rsi, rdx
-        mov     rdi, rax
-        call    strcat
-.L23:
-        mov     rax, [rbp-16]
-        mov     rax, [rax]
-        mov     [rbp-16], rax
-.L22:
-        cmp     qword [rbp-16], 0
-        jne     .L24
-        mov     rax, [rbp-24]
-.L17:
-        leave
-        ret
+    push    rbp
+    mov     rbp, rsp
+    test    rdi, rdi
+    je      .fail
+    test    rdx, rdx
+    je      .fail
+
+    mov     r8, rdi                  ; list
+    mov     r9b, sil                 ; type
+    mov     r10, rdx                 ; base string
+
+    ; strlen(base string)
+    mov     rdi, r10
+    call    strlen
+    mov     r11, rax                 ; total length
+    add     r11, 1                   ; for null terminator
+
+    ; recorrer la lista
+    mov     rbx, [r8]                ; node = list->first
+.loop_len:
+    test    rbx, rbx
+    je      .alloc_buffer
+    movzx   eax, byte [rbx+16]       ; node->type
+    cmp     al, r9b
+    jne     .next_node
+    mov     rdi, [rbx+24]
+    test    rdi, rdi
+    je      .next_node
+    call    strlen
+    add     r11, rax
+.next_node:
+    mov     rbx, [rbx]               ; node = node->next
+    jmp     .loop_len
+
+.alloc_buffer:
+    mov     rdi, r11
+    call    malloc
+    test    rax, rax
+    je      .fail
+    mov     r12, rax                 ; buffer ptr
+    mov     byte [r12], 0            ; inicializa string vacía
+
+    ; strcat(base string)
+    mov     rdi, r12
+    mov     rsi, r10
+    call    strcat
+
+    ; repetir el loop para concatenar
+    mov     rbx, [r8]
+.loop_cat:
+    test    rbx, rbx
+    je      .done
+    movzx   eax, byte [rbx+16]
+    cmp     al, r9b
+    jne     .next_cat
+    mov     rsi, [rbx+24]
+    test    rsi, rsi
+    je      .next_cat
+    mov     rdi, r12
+    call    strcat
+.next_cat:
+    mov     rbx, [rbx]
+    jmp     .loop_cat
+
+.done:
+    mov     rax, r12
+    pop     rbp
+    ret
+.fail:
+    xor     rax, rax
+    pop     rbp
+    ret
 
