53173a426381b2b1545443a5fd0e30b39824caca
Amato de Lusarreta, Franco <famatodelusarreta@udesa.edu.ar>
2025-05-08 11:49:23 -0300

    Update file.c

--- TP3-FileSystem/file.c
+++ TP3-FileSystem/file.c
@@ -9,30 +9,29 @@
  * Lee un bloque de datos del archivo identificado por inumber.
  */
 int file_getblock(struct unixfilesystem *fs, int inumber, int blockNum, void *buf) {
-    struct inode inp;
-    if (inode_iget(fs, inumber, &inp) != 0) {
-        return -1;  // Error al obtener el inodo
-    }
-
-    int blockAddr = inode_indexlookup(fs, &inp, blockNum);
-    if (blockAddr < 0) {
-        return -1;  // Error al buscar el bloque de datos
-    }
-
-    if (diskimg_readsector(fs->dfd, blockAddr, buf) != 0) {
-        return -1;  // Error al leer el bloque de datos
-    }
-
-    int filesize = inode_getsize(&inp);
-    if(filesize < 0){
+    struct inode in;
+    if (inode_iget(fs, inumber, &in) < 0) {
         return -1;
     }
 
-    int startByte = blockNum * DISKIMG_SECTOR_SIZE;
-    if (startByte >= filesize){
-        return 0;
+    int diskBlock = inode_indexlookup(fs, &in, blockNum);
+    if (diskBlock < 0) {
+        return -1;
     }
 
-    int bytesLeft = filesize - startByte;
-    return (bytesLeft >= DISKIMG_SECTOR_SIZE) ? DISKIMG_SECTOR_SIZE : bytesLeft;
+    int res = diskimg_readsector(fs->dfd, diskBlock, buf);
+    if (res != DISKIMG_SECTOR_SIZE) {
+        return -1;
+    }
+
+    int filesize = inode_getsize(&in);
+    int offset = blockNum * DISKIMG_SECTOR_SIZE;
+    if (filesize > offset) {
+        int valid_bytes = filesize - offset;
+        if (valid_bytes > DISKIMG_SECTOR_SIZE) valid_bytes = DISKIMG_SECTOR_SIZE;
+        return valid_bytes;
+
+    } else {
+        return 0;
+    }
 }
