5ae2d604781746dca87f3dad8ae99f5a648a0e67
Amato de Lusarreta, Franco <famatodelusarreta@udesa.edu.ar>
2025-05-13 15:45:15 -0300

    anda?

--- TP3-FileSystem/pathname.c
+++ TP3-FileSystem/pathname.c
@@ -1,37 +1,38 @@
-#include <string.h>
-#include <stdio.h>
+
 #include "pathname.h"
 #include "directory.h"
 #include "inode.h"
+#include "diskimg.h"
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
 
-#define ROOT_INODE_NUMBER 1
-
+/**
+ * TODO
+ */
 int pathname_lookup(struct unixfilesystem *fs, const char *pathname) {
-    if (pathname == NULL || strlen(pathname) == 0) {
-        fprintf(stderr, "Path vacío o nulo\n");
+    if (!pathname || pathname[0] != '/') {
+        // Solo soporta pathnames absolutos
         return -1;
     }
 
-    char path_copy[1024];
+    // Comenzar desde el inodo raíz (usualmente 1)
+    int curr_inumber = 1;
+    char path_copy[256];
     strncpy(path_copy, pathname, sizeof(path_copy));
     path_copy[sizeof(path_copy) - 1] = '\0';
 
-    int current_inumber = ROOT_INODE_NUMBER;
+    // Saltar el primer '/'
     char *token = strtok(path_copy, "/");
-
     while (token != NULL) {
-        struct direntv6 dir_entry;
-
-        fprintf(stderr, "Buscando '%s' en inodo %d\n", token, current_inumber);
-
-        if (directory_findname(fs, token, current_inumber, &dir_entry) < 0) {
-            fprintf(stderr, "No se encontró el componente '%s' en inodo %d\n", token, current_inumber);
-            return -1;
+        struct direntv6 entry;
+        int res = directory_findname(fs, token, curr_inumber, &entry);
+        if (res < 0) {
+            return -1; // No se encontró el componente
         }
-
-        current_inumber = dir_entry.d_inumber;
+        curr_inumber = entry.d_inumber;
         token = strtok(NULL, "/");
     }
 
-    return current_inumber;
+    return curr_inumber;
 }
